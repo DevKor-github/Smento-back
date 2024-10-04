3c47a415effd158d6a4de615868e3d374311d1fe
jjjh02 <jinnyshur@korea.ac.kr>
2024-10-04 17:18:37 +0900

    first setting

---
+++ ontime-back/src/main/java/devkor/ontime_back/OntimeBackApplication.java
@@ -0,0 +1,13 @@
+package devkor.ontime_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntimeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntimeBackApplication.class, args);
+	}
+
+}
---
+++ ontime-back/src/test/java/devkor/ontime_back/OntimeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package devkor.ontime_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntimeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
