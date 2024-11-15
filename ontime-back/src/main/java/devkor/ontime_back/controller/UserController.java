991ba7d0130e9c464b21fd3a57fe7b12fcc11a10
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2024-11-15 23:03:05 +0900

    feat:: 성실도 페이지 GET 기능 구현
    - Schedule엔티티에 지각시간(latenessTime) 속성 추가
    - 성실도페이지, 지각히스토리, 약속히스토리 Response DTO 정의
    - 성실도 페이지 GET기능 컨트롤러, 서비스, 리포지토리 계층에 구현
      (UserController, UserService, ScheduleService, ScheduleRepository)

--- ontime-back/src/main/java/devkor/ontime_back/controller/UserController.java
+++ ontime-back/src/main/java/devkor/ontime_back/controller/UserController.java
@@ -1,5 +1,8 @@
 package devkor.ontime_back.controller;
 
+import devkor.ontime_back.dto.LatenessHistoryResponse;
+import devkor.ontime_back.dto.PunctualityPageResponse;
+import devkor.ontime_back.dto.ScheduleHistoryResponse;
 import devkor.ontime_back.service.ScheduleService;
 import devkor.ontime_back.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
