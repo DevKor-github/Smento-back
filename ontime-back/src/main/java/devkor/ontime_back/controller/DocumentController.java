2915bb27b6dc5c92bf802c2b37fe7cb584696311
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2024-11-27 17:22:03 +0900

    feat::온타임 소개글 조회 API 구현

--- ontime-back/src/main/java/devkor/ontime_back/controller/DocumentController.java
+++ ontime-back/src/main/java/devkor/ontime_back/controller/DocumentController.java
@@ -72,4 +72,34 @@ public class DocumentController {
 
         return ResponseEntity.ok(ApiResponseForm.success(terms, message));
     }
+
+
+    @Operation(
+            summary = "온타임 소개글 조회",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "온타임 소개글 조회 요청 JSON 데이터는 없음. 헤더에 토큰만 있으면 됨",
+                    content = @Content(
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{}"
+                            )
+                    )
+            )
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "온타임 소개글 조회 성공", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            example = "{\n  \"status\": \"success\",\n  \"code\": \"200\",\n  \"message\": \"온타임 소개글 조회 성공\",\n  \"data\": \"온타임은 지각방지 어플리케이션으로 사용자분들이 바쁜 현대사회에서 여유를 찾을 수 있게 도와드립니다.\"\n}"
+                    )
+            )),
+            @ApiResponse(responseCode = "4XX", description = "개인정보처리방침 조회 실패", content = @Content(mediaType = "application/json", schema = @Schema(example = "실패 메세지(정확히 어떤 메세지인지는 모름)")))
+    })
+    @GetMapping("/Ontime-Description")
+    public ResponseEntity<ApiResponseForm<?>> getOntimeDescription() {
+        String terms = "온타임은 지각방지 어플리케이션으로 사용자분들이 바쁜 현대사회에서 여유를 찾을 수 있게 도와드립니다.";
+        String message = "온타임 소개글 조회 성공";
+
+        return ResponseEntity.ok(ApiResponseForm.success(terms, message));
+    }
 }
