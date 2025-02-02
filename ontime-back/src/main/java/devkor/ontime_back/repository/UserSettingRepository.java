9174996ceec36d540b63ae941d3310635c0e840d
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-02-02 21:39:40 +0900

    mod:: UserSettingRepository의 findByUserId메서드에 페치조인 적용

--- ontime-back/src/main/java/devkor/ontime_back/repository/UserSettingRepository.java
+++ ontime-back/src/main/java/devkor/ontime_back/repository/UserSettingRepository.java
@@ -2,6 +2,8 @@ package devkor.ontime_back.repository;
 
 import devkor.ontime_back.entity.UserSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +11,11 @@ import java.util.UUID;
 
 @Repository
 public interface UserSettingRepository extends JpaRepository<UserSetting, UUID> {
-    Optional<UserSetting> findByUserId(Long userId);
+
+    // 페치조인 적용
+    @Query("SELECT us FROM UserSetting us JOIN FETCH us.user WHERE us.user.id = :userId")
+    Optional<UserSetting> findByUserId(@Param("userId") Long userId);
+
+
     Optional<UserSetting> findByUserSettingId(UUID userSettingId);
 }
