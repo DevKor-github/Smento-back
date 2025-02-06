d9b76f435f986a8f07345fc64f5e11a662bac220
jjjh02 <jinnyshur@korea.ac.kr>
2025-02-06 21:46:43 +0900

    mod::소셜로그인 응답 수정

--- ontime-back/src/main/java/devkor/ontime_back/global/oauth/filter/GoogleLoginFilter.java
+++ ontime-back/src/main/java/devkor/ontime_back/global/oauth/filter/GoogleLoginFilter.java
@@ -61,12 +61,14 @@ public class GoogleLoginFilter extends AbstractAuthenticationProcessingFilter {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
+        String msg = user.getRole().name().equals("GUEST") ? "유저의 ROLE이 GUEST이므로 온보딩API를 호출해 온보딩을 진행해야합니다." : "로그인에 성공하였습니다.";
+        // JSON 응답 생성
         String responseBody = String.format(
-                "{\"message\": \"%s\", \"role\": \"%s\"}",
-                user.getRole().name().equals("GUEST")
-                        ? "유저의 ROLE이 GUEST이므로 온보딩API를 호출해 온보딩을 진행해야합니다."
-                        : "로그인에 성공하였습니다.",
-                user.getRole().name()
+                "{ \"status\": \"success\", \"code\": \"200\", \"message\": \"%s\", \"data\": { " +
+                        "\"userId\": %d, \"email\": \"%s\", \"name\": \"%s\", " +
+                        "\"spareTime\": \"%s\", \"note\": \"%s\", \"punctualityScore\": %f, \"role\": \"%s\" } }",
+                msg, user.getId(), user.getEmail(), user.getName(),
+                user.getSpareTime(), user.getNote(), user.getPunctualityScore(), user.getRole().name()
         );
 
         response.getWriter().write(responseBody);
