002ae700f9d69f8f4492ffde15b6eaecb90fc905
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-02-09 13:07:37 +0900

    mod:: 로그인시 note값이 null일 때 반환되는 형식 수정("null" -> null)

--- ontime-back/src/main/java/devkor/ontime_back/global/generallogin/handler/LoginSuccessHandler.java
+++ ontime-back/src/main/java/devkor/ontime_back/global/generallogin/handler/LoginSuccessHandler.java
@@ -58,9 +58,9 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
                         String responseBody = String.format(
                                 "{ \"status\": \"success\", \"code\": \"200\", \"message\": \"%s\", \"data\": { " +
                                         "\"userId\": %d, \"email\": \"%s\", \"name\": \"%s\", " +
-                                        "\"spareTime\": %d, \"note\": \"%s\", \"punctualityScore\": %f, \"role\": \"%s\" } }",
+                                        "\"spareTime\": %d, \"note\": %s, \"punctualityScore\": %f, \"role\": \"%s\" } }",
                                 msg, user.getId(), user.getEmail(), user.getName(),
-                                user.getSpareTime(), user.getNote(), user.getPunctualityScore(), user.getRole().name()
+                                user.getSpareTime(), user.getNote() != null ? "\"" + user.getNote() + "\"" : null, user.getPunctualityScore(), user.getRole().name()
                         );
 
                         // 응답 바디에 작성
